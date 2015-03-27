02e98e62278418028cdf0daf710a52baad493cac
Albert Liang <aliang@almondcentos6.(none)>
2015-03-26 20:34:47 -0700

    Working Dice Roll Thinking about adding a "write to file" capability

---
+++ DiceRoll.py
@@ -0,0 +1,48 @@
+"""
+Dice Roll
+
+3/26/15
+Albert Liang
+
+Simulates the rolling of any number of six-sided dice.
+User selects how many dice to roll at the start of each iteration.
+The dice are rolled and recorded as a list, which is then printed to the console.
+The program loops until the user enters "q" instead of a dice count.
+"""
+from random import *
+
+# Main Program =================================================================
+def main():
+    print "Welcome to Dice Roll!\n"
+
+    while(True):
+        count = raw_input("How many dice? (\"q\" to quit): ")
+        if( count.lower() == 'q' ):
+            break
+        elif( not( StringRepInt(count) ) ):
+            print( "Please enter an integer count, or q.\n" )
+        elif( int(count) < 1 ):
+	    print( "Please enter a dice count greater than zero.\n" )
+        else:
+            result = []
+            for i in range( int(count) ):
+	        roll = randint(1,6)
+	        result.append(roll)
+	    print "Results: ", result, "\n"
+
+    print ("Exiting...\n\n")
+
+# Check if String Represents an Integer ========================================
+# (From Stackoverflow)
+def StringRepInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+# ==============================================================================
+# Run ==========================================================================
+main()
+
