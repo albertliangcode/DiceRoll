074531c1da01988d59ef7c4b7f0014e5a5c09d78
Albert Liang <albertliangcode@gmail.com>
2015-03-26 18:06:24 -0700

    Correction on Test Can use "from ___ import ___", as long as I remove period syntax :

--- datetime.py
+++ datetime.py
@@ -7,12 +7,12 @@ Albert Liang
 Importing the time module, print the current global and local date and time to console.
 """
 
-import time
+from time import *
 
-gnow = time.gmtime(time.time())
-lnow = time.localtime(time.time())
+gnow = gmtime(time())
+lnow = localtime(time())
 	#What's with the weird nested time functions?
 
-print "Global:\t", time.asctime(gnow)
-print "Local:\t", time.asctime(lnow)
+print "Global:\t", asctime(gnow)
+print "Local:\t", asctime(lnow)
 
